ff58e55d4b02ae978f4ac6e78b1c3085df41321c
herofahad777 <shagufi.mir@gmail.com>
2023-01-03 22:03:02 +0530

    Changing back to v4.0 or offline downloading file

--- src/main/java/net/mcreator/djmusicalanwalkerplaylist/procedure/ProcedureFolderCreater.java
+++
@@ -1,19 +0,0 @@
-package net.mcreator.djmusicalanwalkerplaylist.procedure;
-
-import net.mcreator.djmusicalanwalkerplaylist.ElementsDjMusicAlanWalkerPlaylistMod;
-
-import java.util.Map;
-import java.io.*;
-@ElementsDjMusicAlanWalkerPlaylistMod.ModElement.Tag
-public class ProcedureFolderCreater extends ElementsDjMusicAlanWalkerPlaylistMod.ModElement {
-	public ProcedureFolderCreater(ElementsDjMusicAlanWalkerPlaylistMod instance) {
-		super(instance, 43);
-	}
-
-	public static void executeProcedure(Map<String, Object> dependencies){
-      File path = new File("songs").getAbsoluteFile();
-      boolean bool = path.mkdir();  
-      if(bool) 
-         System.out.println("Folder is created successfully");      
-	}
-}
